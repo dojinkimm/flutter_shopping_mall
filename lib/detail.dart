f5de81be49ddcd45657f4bb867a92889c4681a85
Henry <dojinkim119@gmail.com>
2018-11-12 22:20:18 +0900

    only if current user is creator of the file can edit or delete

--- lib/detail.dart
+++ lib/detail.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:shopping_mall/edit.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class DetailPage extends StatefulWidget {
   final pid;
@@ -12,6 +15,52 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  String creator;
+  bool doOrNot;
+
+  @override
+  void initState() {
+    super.initState();
+    doOrNot = false;
+    creator = "";
+  }
+
+  Future _showDialog(BuildContext context, String editOrDelete) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Really want to do this?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Close"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              FlatButton(
+                child: Text("YES"),
+                onPressed: () {
+                  if (editOrDelete == "edit") {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => EditPage(pid: widget.pid)));
+                  } else if (editOrDelete == "delete") {
+                    print("deleting");
+                    Firestore.instance
+                        .collection('product')
+                        .document(widget.pid)
+                        .delete().then((val){
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        });
+                  }
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,12 +69,26 @@ class _DetailPageState extends State<DetailPage> {
         title: Text("Detail Page"),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=>EditPage())),
-          ),
+              icon: Icon(Icons.edit),
+              onPressed: () async {
+                final FirebaseUser currentUser = await _auth.currentUser();
+                if (currentUser.uid == creator) {
+                  _showDialog(context, "edit");
+                } else {
+                  print("wrong user");
+                }
+              }),
           IconButton(
-            icon: Icon(Icons.remove_circle),
-            onPressed: () {},
+            icon: Icon(Icons.delete),
+            onPressed: () async {
+              final FirebaseUser currentUser = await _auth.currentUser();
+              if (currentUser.uid == creator) {
+                _showDialog(context, "delete");
+              } else {
+                print("wrong user");
+              }
+            
+            },
           ),
         ],
       ),
@@ -40,35 +103,47 @@ class _DetailPageState extends State<DetailPage> {
               return Center(child: CircularProgressIndicator());
             else {
               final DocumentSnapshot document = snapshot.data.documents[0];
+
+              creator = document['creator'];
+
               return ListView(children: <Widget>[
                 Image.network(
                   document['imageURL'],
                   fit: BoxFit.fitWidth,
                 ),
                 Container(
-                  height: MediaQuery.of(context).size.height*0.4,
-                  padding: EdgeInsets.only(top: 50.0, left: 30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(document['name'], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.blue),),
-                      SizedBox(height: 10.0,),
-                      Text("\$${document['price'].toString()}", style: TextStyle(color: Colors.blue[400], fontSize: 18.0)),
-                      Expanded(child: Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Text("creator: ${widget.pid}"),
-                            Text("Created"),
-                            Text("Modified"),
-                          ],
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    padding: EdgeInsets.only(top: 50.0, left: 30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          document['name'],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                              color: Colors.blue),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text("\$${document['price'].toString()}",
+                            style: TextStyle(
+                                color: Colors.blue[400], fontSize: 18.0)),
+                        Expanded(
+                          child: Container(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text("Creator: ${document['creator']}"),
+                              Text("Created: ${document['created']}"),
+                              Text("Modified: ${document['modified']}"),
+                            ],
+                          )),
                         )
-                      ),)
-                    ],
-                  )
-                ),
-
+                      ],
+                    )),
               ]);
             }
           }),
